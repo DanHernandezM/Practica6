d7f36f0eea78da15163977edfb7350cc20163092
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-21 19:45:00 -0600

    Fin de la practica

--- src/graphics/Rectangulo.java
+++ src/graphics/Rectangulo.java
@@ -1,5 +1,8 @@
 package graphics;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  * @author Héctor Quej Cosgaya
  * @author Jose Aguilar Canepa
@@ -7,5 +10,58 @@ package graphics;
  * Esta clase necesita de tu ayuda!
  */
 public class Rectangulo extends FiguraGrafica {
+    protected int x;
+    protected int y;
+    protected int lado1;
+    protected int lado2;
+
+    public Rectangulo (int x, int y, int lado1, int lado2) {
+        this.x = x;
+        this.y = y;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
     
+    @Override
+    public void dibujar(Graphics g){
+        g.fillRect(x, y, lado1, lado2);
+    }
+    
+    @Override
+    public void encojer(Graphics g) {
+        lado1 -= 10;
+        if (lado1 < 20) lado1 = 20;
+        
+        lado2 -= 5;
+        if (lado2 < 10) lado2 = 10;
+        
+        this.dibujar(g);        
+    }
+    
+    @Override
+    public void agrandar(Graphics g) {
+        lado1 += 10;
+        if (lado1 > 300) lado1 = 300;
+        this.dibujar(g);
+        lado2 += 5;
+        if (lado2 > 150) lado2 = 150;
+        this.dibujar(g);
+    }
+    
+    @Override
+    public void cambiarColor(Graphics g, Color c) {
+        g.setColor(c);
+        this.dibujar(g);
+    }
+    
+    @Override
+    public void mover(Graphics g, String direccion){
+        switch(direccion){
+            case "arriba"    : y-=5; break;
+            case "abajo"     : y+=5; break;
+            case "izquierda" : x-=5; break;
+            case "derecha"   : x+=5; break;
+        }
+        this.dibujar(g);
+    }    
 }
--- src/gui/PoliVisor.java
+++ src/gui/PoliVisor.java
@@ -2,6 +2,8 @@ package gui;
 
 import graphics.Cuadrado;
 import graphics.FiguraGrafica;
+import graphics.Triangulo;
+import graphics.Rectangulo;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JColorChooser;
@@ -46,7 +48,7 @@ public class PoliVisor extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        panelTipo.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "¿Qué deseas dibujar?", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Comic Sans MS", 0, 14), java.awt.Color.black)); // NOI18N
+        panelTipo.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "¿Qué deseas dibujar?", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Comic Sans MS", 0, 14))); // NOI18N
 
         botonCuadrado.setText("Un cuadrado!");
         botonCuadrado.addActionListener(new java.awt.event.ActionListener() {
@@ -56,8 +58,18 @@ public class PoliVisor extends javax.swing.JFrame {
         });
 
         botonRectangulo.setText("Un rectángulo!");
+        botonRectangulo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonRectanguloActionPerformed(evt);
+            }
+        });
 
         botonTriangulo.setText("Un triángulo!");
+        botonTriangulo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonTrianguloActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout panelTipoLayout = new javax.swing.GroupLayout(panelTipo);
         panelTipo.setLayout(panelTipoLayout);
@@ -83,7 +95,7 @@ public class PoliVisor extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        panelControl.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, null, java.awt.Color.black));
+        panelControl.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
         botonArriba.setText("^");
         botonArriba.setEnabled(false);
@@ -159,7 +171,7 @@ public class PoliVisor extends javax.swing.JFrame {
                 .addGroup(panelControlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(botonDerecha, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(botonGrande, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         panelControlLayout.setVerticalGroup(
             panelControlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -258,6 +270,18 @@ public class PoliVisor extends javax.swing.JFrame {
         figura.dibujar(getPanelGraphics());
         enableControls();
     }//GEN-LAST:event_botonCuadradoActionPerformed
+
+    private void botonTrianguloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonTrianguloActionPerformed
+    figura = new Triangulo();
+    figura.dibujar(getPanelGraphics());
+    enableControls();
+    }//GEN-LAST:event_botonTrianguloActionPerformed
+
+    private void botonRectanguloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonRectanguloActionPerformed
+    figura = new Rectangulo(15, 15, 100, 50);
+        figura.dibujar(getPanelGraphics());
+        enableControls();
+    }//GEN-LAST:event_botonRectanguloActionPerformed
 
     /**
      * @param args the command line arguments
